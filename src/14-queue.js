6ceea1601826fe2eb36b57fc84a1e060ca93834e
Vaganov Anil <vag-anil@yandex.ru>
2021-04-12 23:36:38 +0500

    adding solution 14

--- src/14-queue.js
+++ src/14-queue.js
@@ -11,17 +11,39 @@
  *
  */
 
-class Queue {
-  get size() {
-    throw new Error('Not implemented');
+ class Queue {
+  constructor() { // конструктор класса как в предыдущем
+    this.head = null; // начальный указатель на список
+    this.length = 0; // добавил счетчик списка чтобы не пробегать каждый раз и не считать длину
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  get size() {
+    return this.length; // собственно всегда возвращаю просто значение длины
+  }
+
+  enqueue(element) {
+    if (this.length === 0) {
+      this.head = new ListNode(element); // если список пустой, тогда добавление элемента - это ссылка начального head на добавляемый элемент
+      // элемент должен создаваться при помощи функции ListNode которая создана разработчиками теста. Она подключается в 1 строке файла
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+        // если списое не пустой, тогда идем по скиску последовательно пока не дойдем до конца
+      }
+
+      current.next = new ListNode(element); // когда дошли до конца - добавляем новый элемент в конец.
+    }
+
+    this.length++; // увеличиваем счетчик на 1
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    if (this.head === null) return null; // если список пустой - нечего возвращать
+    const deletedElement = this.head.value; // запоминаем первый элемент чтобы после удаления можно было его вернуть как результат выполнения метода
+    this.head = this.head.next;  // удаляем первый элемент путем смещения начальной ссылки head на второй элемент. первый элемент таким образом удаляется
+    this.length--;  // уменьшаем длину списка
+    return deletedElement;  // возвращаем элемент, который удалили
   }
 }
 
